19c793ec75ebf18997bbb36a5f8d471cd531a919
Chikennannbann <kamsk517@gmail.com>
2022-04-28 03:14:21 +0000

    [finish]応用課題4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,15 @@
 class RelationshipsController < ApplicationController
+   before_action :authenticate_user!
 
    def create
-     current_user.follow(params[:user_id])
+     user = User.find(params[:user_id])
+     current_user.follow(user)
      redirect_to request.referer
    end
 
    def destroy
-     current_user.unfollow(params[:user_id])
+     user = User.find(params[:user_id])
+     current_user.unfollow(user)
      redirect_to request.referer
    end
 
@@ -19,7 +22,7 @@ class RelationshipsController < ApplicationController
    # フォロワー一覧
    def followers
       user = User.find(params[:user_id])
-      @user = user.followers
+      @users = user.followers
    end
 
 end
