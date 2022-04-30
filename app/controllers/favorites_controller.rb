d0027e8d324b98e32dcfcd13c100221d632c37ba
Chikennannbann <kamsk517@gmail.com>
2022-04-30 08:29:26 +0000

    [add]応用課題6反映以外

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,16 +4,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back(fallback_location: books_path)
-    # fallbackで戻れなかった時用のパスを指定
-    # redirect_to request.refererも同じ
+    # redirect_back(fallback_location: books_path)
+    # # fallbackで戻れなかった時用のパスを指定
+    # # redirect_to request.refererも同じ
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
   end
 
 end
