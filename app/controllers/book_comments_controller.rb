64dc32122174d4fea80d9e74ca55a8419fd4bc7f
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-05-04 08:51:34 +0000

    [finish]応用課題6comment非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,20 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
     # comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: book_path(book))
+    @comment.book_id = book.id
+    @comment.save
+    # redirect_back(fallback_location: book_path(book))
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
     # BookComment.find(params[:id]).destroyでもok
     # find_byはidとbook_idで探す。URIパターンがそうだからこのように書いていると考えられる。
-    redirect_back(fallback_location: book_path(params[:book_id]))
+    # redirect_back(fallback_location: book_path(params[:book_id]))
   end
 
   private
